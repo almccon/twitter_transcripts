4c294ca6c06b58eb0306e94f3dc2436cf587a375
Alan McConchie <alan.mcconchie@gmail.com>
2013-03-30 18:21:56 -0700

    Changed indents

--- process_hootsuite.py
+++ process_hootsuite.py
@@ -20,56 +20,56 @@ lines = p.split(string)
 lines.reverse() # reverses in place
 
 for line in lines:
-  # I should probably move all these compile stmts outside the loop
+    # I should probably move all these compile stmts outside the loop
 
-  if (debug): 
-    print line  # debug
+    if (debug): 
+        print line  # debug
 
-  p = re.compile('lazysrc')
-  line = p.sub('src', line)
+    p = re.compile('lazysrc')
+    line = p.sub('src', line)
 
-  p = re.compile(' class="networkAvatar.*?"') # .*? is non-greedy version of .*
-  line = p.sub('', line)
+    p = re.compile(' class="networkAvatar.*?"') # .*? is non-greedy version of .*
+    line = p.sub('', line)
 
-  p = re.compile('^.*<img src') # ^ to match beginning of string
-  line = p.sub('<br clear=all><p><img height="48px" align="left" src', line)
+    p = re.compile('^.*<img src') # ^ to match beginning of string
+    line = p.sub('<br clear=all><p><img height="48px" align="left" src', line)
 
-  p = re.compile('<p class.*?>') # .*? is non-greedy version of .*
-  line = p.sub('', line)
+    p = re.compile('<p class.*?>') # .*? is non-greedy version of .*
+    line = p.sub('', line)
 
-  p = re.compile('<a href="#" class="_user.*?>') # .*? is non-greedy
-  line = p.sub('', line)
+    p = re.compile('<a href="#" class="_user.*?>') # .*? is non-greedy
+    line = p.sub('', line)
 
-  p = re.compile('<a href="#" class="_quick.*?>') # .*? is non-greedy
-  line = p.sub('', line)
+    p = re.compile('<a href="#" class="_quick.*?>') # .*? is non-greedy
+    line = p.sub('', line)
 
-  # now to remove the </a> after people's names
-  p = re.compile('>\s+(\S+)<\/a>\s\s\s') # + requires at least one is non-greedy
-  line = p.sub('> @\\1', line, 1) # The trailing 1 stops this after the 1st sub
+    # now to remove the </a> after people's names
+    p = re.compile('>\s+(\S+)<\/a>\s\s\s') # + requires at least one is non-greedy
+    line = p.sub('> @\\1', line, 1) # The trailing 1 stops this after the 1st sub
 
-  # Also do this, to remove </a> after handles and hashtags in body of tweets:
-  p = re.compile('([@#]\S*)<\/a>') 
-  line = p.sub('\\1', line)
+    # Also do this, to remove </a> after handles and hashtags in body of tweets:
+    p = re.compile('([@#]\S*)<\/a>') 
+    line = p.sub('\\1', line)
 
-  # remove junk from the end:
-  p = re.compile('<\/p>.*<\/div>') 
-  line = p.sub('', line)
+    # remove junk from the end:
+    p = re.compile('<\/p>.*<\/div>') 
+    line = p.sub('', line)
 
-  # remove the "via" stuff:
-  p = re.compile('via\s\s\s\s.*?a>') 
-  line = p.sub('</a>', line)
+    # remove the "via" stuff:
+    p = re.compile('via\s\s\s\s.*?a>') 
+    line = p.sub('</a>', line)
+  
+    # remove <a class="_previewLink...  
+    p = re.compile('<\/a><a href.*?class="_previewLink.*?><\/a>') 
+    line = p.sub('</a>', line)
 
-  # remove <a class="_previewLink...  
-  p = re.compile('<\/a><a href.*?class="_previewLink.*?><\/a>') 
-  line = p.sub('</a>', line)
+    # clean up the stupid "..." symbol
+    #p = re.compile(u"\xe2") 
+    p = re.compile('…') 
+    line = p.sub('...', line)
 
-  # clean up the stupid "..." symbol
-  #p = re.compile(u"\xe2") 
-  p = re.compile('…') 
-  line = p.sub('...', line)
+    print line
 
-  print line
-
-  if (debug): 
-    print "\n"
+    if (debug): 
+        print "\n"
 
--- tweet_stats.py
+++ tweet_stats.py
@@ -12,33 +12,33 @@ debug = False
 
 for line in fileinput.input():
 
-  if (debug): 
-    print line  # debug
+    if (debug): 
+        print line  # debug
+    
+    # extract the date from the filename
 
-  # extract the date from the filename
+    filename = fileinput.filename()
+    p = re.compile('\d\d\d\d-\d\d-\d\d')  
+    date = p.search(filename)
 
-  filename = fileinput.filename()
-  p = re.compile('\d\d\d\d-\d\d-\d\d')  
-  date = p.search(filename)
+    # catch the first twitter handle (the author)
+    # for the old-style logs, the first handle is missing the "@"
 
-  # catch the first twitter handle (the author)
-  # for the old-style logs, the first handle is missing the "@"
+    p = re.compile('.*?<img.*?>\s*@?(\w+)(\s|<)')  
+    handle = p.search(line)
 
-  p = re.compile('.*?<img.*?>\s*@?(\w+)(\s|<)')  
-  handle = p.search(line)
-
-  # match the date, old hootsuite styles (Tue Aug 02 18:08:21 +0000 2011), (11:54 AM Jul 26th, 2011) 
-  # or new style (2013-02-05T12:06:09)... or failing that, match (1:03pm)
-  # For now, just leave the timestamp in all these different styles... deal with it later
+    # match the date, old hootsuite styles (Tue Aug 02 18:08:21 +0000 2011), (11:54 AM Jul 26th, 2011) 
+    # or new style (2013-02-05T12:06:09)... or failing that, match (1:03pm)
+    # For now, just leave the timestamp in all these different styles... deal with it later
   
-  p = re.compile('(\w\w\w \w\w\w \d\d \d\d:\d\d:\d\d \+\d\d\d\d \d\d\d\d)|(\d\d:\d\d \w\w \w\w\w \d?\d\w\w, \d\d\d\d)|(\d\d\d\d-\d\d-\d\d\w\d\d:\d\d:\d\d)|(\d?\d:\d\d(a|p)m)') 
-  timestamp = p.search(line)
+    p = re.compile('(\w\w\w \w\w\w \d\d \d\d:\d\d:\d\d \+\d\d\d\d \d\d\d\d)|(\d\d:\d\d \w\w \w\w\w \d?\d\w\w, \d\d\d\d)|(\d\d\d\d-\d\d-\d\d\w\d\d:\d\d:\d\d)|(\d?\d:\d\d(a|p)m)') 
+    timestamp = p.search(line)
 
-  if handle and timestamp:
-    print "\t".join((handle.group(1), date.group(), timestamp.group()))
-  else:
-    print "COULDN'T PARSE:", line
+    if handle and timestamp:
+        print "\t".join((handle.group(1), date.group(), timestamp.group()))
+    else:
+        print "COULDN'T PARSE:", line
 
-  if (debug): 
-    print "\n"
+    if (debug): 
+        print "\n"
 
