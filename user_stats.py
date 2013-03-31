6564af1567416094ce2009325a8e2bf15dd0e070
Alan McConchie <alan.mcconchie@gmail.com>
2013-03-30 19:20:55 -0700

    A script to tally tweet counts by user

---
+++ user_stats.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# coding: utf8
+
+import fileinput
+
+userdict = {}
+
+for line in fileinput.input():
+    (username, date, timestamp) = line.split('\t')
+    
+    # test for existence of key. If not present, create empty dict.
+    if not username in userdict:
+        userdict[username] = {}
+    if not date in userdict[username]:
+        userdict[username][date] = 1
+    else:
+        userdict[username][date] += 1
+    
+for username in sorted(userdict):
+    for date in sorted(userdict[username]):
+        print "\t".join((username, date, str(userdict[username][date])))
+
+        
+    
